4631d74b9a660970ba976126a13f2702bca27fa0
Danielle Badobre <badobredanielle@gmail.com>
2023-10-28 21:24:21 +0000

    first python project

---
+++ 0x00-python-hello_world/tests.py
@@ -0,0 +1,9 @@
+age = int(input('Enter your age please: '))
+if age <= 4:
+    print("kindergarden")
+elif age >= 5 and age <= 17:
+    print("va a l'ecole")
+elif age > 17 and age < 25:
+    print("Go to college")
+else:
+    print("you're free bud !")
